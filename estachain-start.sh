b23af5ad12953b6b2655b2ccd5031d2731b32742
shukriev <dark00v3@gmail.com>
2020-01-09 17:00:53 +0200

    Redirect rpc address to localhost

--- estachain-start.sh
+++ estachain-start.sh
@@ -7,7 +7,8 @@ DATADIR=Estachain/estachain-data
 GENESIS=Estachain/genesis.json
 KEYSTORE=keystore
 RPC_PORT=8545
-RPC_ADDR=$(awk 'END{print $1}' /etc/hosts)
+#RPC_ADDR=$(awk 'END{print $1}' /etc/hosts)
+RPC_ADDR=127.0.0.1
 RPC_API="istanbul,clique,console,eth,web3,admin,debug,miner,personal,txpool,net"
 
 # init the data directory
@@ -16,8 +17,11 @@ echo "Estachain INIT $RPC_ADDR"
 $ESTACHAIN --datadir $DATADIR init $GENESIS
 
 # start the node with the keystore and nodekey
+
+#$ESTACHAIN --rpc --rpcport $RPC_PORT --rpcapi "$RPC_API" --rpcaddr $RPC_ADDR --rpccorsdomain "*" --datadir $DATADIR --nousb --networkid 123 --nodiscover --keystore $KEYSTORE
+
 echo "Estachain START"
-$ESTACHAIN --rpc --rpcport $RPC_PORT --rpcapi "$RPC_API" --rpcaddr $RPC_ADDR --rpccorsdomain "*" --datadir $DATADIR --networkid 123 --nodiscover --keystore $KEYSTORE
+$ESTACHAIN --rpc --rpcport $RPC_PORT --rpcapi "$RPC_API" --rpcaddr $RPC_ADDR --rpccorsdomain "*" --datadir $DATADIR --nousb --networkid 123 --nodiscover --keystore $KEYSTORE
   #--syncmode "full" \
   #--mine \
   #--miner.threads 1 \
